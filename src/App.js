86405dc59418baa92b4c3329e68b6273a849ce3f
Nathalie Berwa <nathalieberwa@outlook.fr>
2023-10-25 14:52:54 +0200

    add router packages and some components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+
+import Users from "./users/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
 
 function App() {
-  return <h1> coucou !! </h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact={true}>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact={true}>
+          <NewPlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/users/components/UsersList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserUsersList = () => {
+  return;
+};
+
+export default UserUsersList;
