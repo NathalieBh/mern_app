4e326e3a72051c3450ccd601accff2b365a52aab
Nathalie Berwa <nathalieberwa@outlook.fr>
2023-10-31 11:21:58 +0100

    add userItem, avatar and card components

--- src/users/components/UserItem.js
+++ src/users/components/UserItem.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-import "./UserItem.css";
+import Avatar from '../../shared/components/Avatar';
+import './UserItem.css';
 
-const UserItem = (props) => {};
+const UserItem = (props) => {
+  const STYLE = {
+    width: '100%',
+    height: '100%',
+  };
+  return (
+    <li className='user-item'>
+      <div className='user-item__content'>
+        <Link>
+          <div className='user-item__image'>
+            <Avatar src={props.image} alt={props.name} style={STYLE} />
+          </div>
+
+          <div className='user-item__info'>
+            <h2>{props.name}</h2>
+            <h3>
+              {props.placeCount} {props.placeCount === 1 ? 'place' : 'places'}
+            </h3>
+          </div>
+        </Link>
+      </div>
+    </li>
+  );
+};
 
 export default UserItem;
--- src/users/components/UsersList.js
+++ src/users/components/UsersList.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React from 'react';
 
-const UserUsersList = () => {
-  return;
+import UserItem from './UserItem';
+import './UsersList.css';
+
+const UsersList = (props) => {
+  if (props.items.length === 0) {
+    return (
+      <div className='center'>
+        <h2>no users found.</h2>
+      </div>
+    );
+  }
+  return (
+    <ul className='users-list'>
+      {props.items.map((user) => {
+        return (
+          <UserItem
+            key={user.id}
+            id={user.id}
+            image={user.image}
+            name={user.name}
+            placeCount={user.places}
+          />
+        );
+      })}
+    </ul>
+  );
 };
 
-export default UserUsersList;
+export default UsersList;
